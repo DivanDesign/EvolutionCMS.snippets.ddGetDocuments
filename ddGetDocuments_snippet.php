40cc7d88c27df496306a7adfd6ee3c96d541f075
Pavel <phm.psthmn@gmail.com>
2016-01-29 14:22:06 +0500

    * Prerelease 0.4.1

--- ddGetDocuments_snippet.php
+++ ddGetDocuments_snippet.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ddGetDocuments
- * @version 0.4.0 (2016-01-20)
+ * @version 0.4.1 (2016-01-29)
  * 
  * A snippet for fetching and parsing resources from the document tree by a custom rule.
  * 
