b3e0bab1c3e9c1c16444b452d845191fa339e552
Andrey Demchenko <ehot@divandesign.ru>
2016-06-30 18:17:27 +0500

    + Добавлены комментарии и описания.

--- src/OutputFormat/Json/OutputFormat.php
+++ src/OutputFormat/Json/OutputFormat.php
@@ -11,30 +11,33 @@ class OutputFormat extends \ddGetDocuments\OutputFormat\OutputFormat
 	 * 
 	 * @param Output $data
 	 * @param array $outputFormatParameters
-	 * $outputFormatParameters['TVtoJson']. TV необходимые для json массива. @Default: 'id'
-	 * $outputFormatParameters['wrapResult']. Оборачивает результат в качестве разделителя используется «||». @Default: ''
+	 * $outputFormatParameters['TVtoJson']. TV из которых будет состоять json. @Default: 'id'
+	 * $outputFormatParameters['wrapResult']. Оборачивает результат. В качестве разделителя используется «||». @Default: ''
 	 * 
-	 * @return string
+	 * @return json array
 	 */
 	public function parse(Output $data, array $outputFormatParameters){
 		global $modx;
 		$output = array();
 		$dataArray = $data->toArray();
-		
+		//Проверим заполнен ли параметр
 		$TVtoJson = (isset($outputFormatParameters['TVtoJson']) && $outputFormatParameters['TVtoJson'] != '') ? explode(',',$outputFormatParameters['TVtoJson']) : 'id';
-		
+		//Пройдемся по полученным данным
 		foreach($dataArray['provider']['items'] as $key => $value){
-			
+			//Для каждого найденого id найдем необходимые TV
 			$document = \ddTools::getTemplateVarOutput($TVtoJson, $value['id']);
-			
+			//JSON_UNESCAPED_UNICODE — Не кодировать многобайтные символы Unicode || JSON_UNESCAPED_SLASHES — Не экранировать /
 			$output[] .= json_encode($document, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+			//Добавим запятую после каждого элемента
 			$result = implode(",", $output);
 		}
 		
+		//Обернем если надо
 		if (isset($outputFormatParameters['wrapResult']) && $outputFormatParameters['wrapResult'] != ''){
 			$wrap = explode('||',$outputFormatParameters['wrapResult']);
 			return $wrap[0] .$result. $wrap[1];
 		}else{
+			//Не обернем если не надо
 			return $result;
 		};
 	}
