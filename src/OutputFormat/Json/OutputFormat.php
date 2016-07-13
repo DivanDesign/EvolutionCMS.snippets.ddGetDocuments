ac665b8431c55c832a4d28d639866c25d4d96620
Ilyas Ronef <ilyas@divandesign.biz>
2016-07-13 21:07:36 +0500

    * ddGetDocuments\OutputFormat\Json\OutputFormat → parse: Refactoring output vars.

--- src/OutputFormat/Json/OutputFormat.php
+++ src/OutputFormat/Json/OutputFormat.php
@@ -19,18 +19,16 @@ class OutputFormat extends \ddGetDocuments\OutputFormat\OutputFormat
 		global $modx;
 		$output = array();
 		$dataArray = $data->toArray();
+		
 		//Проверим заполнен ли параметр
 		$TVtoJson = (isset($outputFormatParameters['TVtoJson']) && $outputFormatParameters['TVtoJson'] != '') ? explode(',',$outputFormatParameters['TVtoJson']) : 'id';
 		//Пройдемся по полученным данным
 		foreach($dataArray['provider']['items'] as $key => $value){
 			//Для каждого найденого id найдем необходимые TV
-			$document = \ddTools::getTemplateVarOutput($TVtoJson, $value['id']);
-			//JSON_UNESCAPED_UNICODE — Не кодировать многобайтные символы Unicode || JSON_UNESCAPED_SLASHES — Не экранировать /
-			$output[] .= json_encode($document, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-			//Добавим запятую после каждого элемента
-			$result = implode(",", $output);
+			$output[] = \ddTools::getTemplateVarOutput($TVtoJson, $value['id']);
 		}
 		
-		return $result;
+		//JSON_UNESCAPED_UNICODE — Не кодировать многобайтные символы Unicode || JSON_UNESCAPED_SLASHES — Не экранировать /
+		return json_encode($output, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 	}
 }
