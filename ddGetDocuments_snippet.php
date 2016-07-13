045feaf0f8c9bbe54cb98311b8e0063edb05706d
Ilyas Ronef <ilyas@divandesign.biz>
2016-07-13 22:51:26 +0500

    * Snippet ddGetDocuments: Parameters description enhancement.

--- ddGetDocuments_snippet.php
+++ ddGetDocuments_snippet.php
@@ -5,28 +5,45 @@
  * 
  * A snippet for fetching and parsing resources from the document tree by a custom rule.
  * 
- * @param string $provider - Name of the provider that will be used to fetch documents.
- * @param string $providerParams - Parameters to be passed to the provider. The parameter must be set as a query string,
+ * @param 'parent'|'select' $provider - Name of the provider that will be used to fetch documents. Default: 'parent'.
+ * @param string_query $providerParams - Parameters to be passed to the provider. The parameter must be set as a query string,
+ * When $provider == 'parent' =>
+ * @param array|string_commaSepareted $providerParams['parentIds'] - Parent IDs. Default: 0.
+ * @param integer $providerParams['depth'] - Depth of children documents search. Default: 1.
  * e.g. $providerParams = 'parentIds=1&depth=2'.
  * @param string $fieldDelimiter - The field delimiter to be used in order to distinct data base column names in those
  * parameters which can contain SQL queries directly, e. g. $orderBy and $filter. Default: '`'.
  * 
- * @param integer $total - The maximum number of the resources that will be returned.
+ * @param integer $total - The maximum number of the resources that will be returned. Default: —.
  * @param string $filter - The filter condition in SQL-style to be applied while resource fetching. Default: '`published` = 1 AND `deleted` = 0';
  * Notice that all fields/tvs names specified in the filter parameter must be wrapped in $fieldDelimiter.
- * @param integer $offset - Resources offset.
+ * @param integer $offset - Resources offset. Default: 0.
  * @param string $orderBy - A string representing the sorting rule. Default: '`id` ASC'.
  * 
- * @param 'string'|'raw'|'json'  $outputFormat - Format of the output. Default: 'string'.
- * @param string $outputFormatParams - Parameters to be passed to the specified formatter. The parameter must be set as a query string,
- * e.g. $outputFormatParams = 'itemTpl=chunk_1&wrapperTpl=chunk_2&noResults=No items found'.
- * @param json $outputFormatParams - Parameters to be passed to the specified formatter. The parameter must be set as a query string,
- * e.g. $outputFormatParams = 'docFields=pagetitle,introtext'.
- * docFields - Поля документа, которые попадут в json массив. @Default: 'id'
+ * @param 'string'|'json'|'raw' $outputFormat - Format of the output. Default: 'string'.
+ * @param string_query $outputFormatParams - Parameters to be passed to the specified formatter. The parameter must be set as a query string,
+ * When $outputFormat == 'string' =>
+ * @param string_chunkName $outputFormatParams['itemTpl'] - Item template. Available placeholders: [+any field or tv name+], [+any of extender placeholders+]. @required
+ * @param string_chunkName $outputFormatParams['itemTplFirst'] - First item template. Available placeholders: [+any field or tv name+], [+any of extender placeholders+].
+ * @param string_chunkName $outputFormatParams['itemTplLast'] - Last item template. Available placeholders: [+any field or tv name+], [+any of extender placeholders+].
+ * @param string_chunkName $outputFormatParams['wrapperTpl'] - Wrapper template. Available placeholders: [+ddGetDocuments_items+], [+any of extender placeholders+].
+ * @param string|string_chunkName $outputFormatParams['noResults'] - A chunk or text to output when no items found. Available placeholders: [+any of extender placeholders+]. 
+ * e.g. 'itemTpl=chunk_1&wrapperTpl=chunk_2&noResults=No items found'.
+ * When $outputFormat == 'json' =>
+ * @param string_commaSeparated $outputFormatParams['docFields'] - Document fields to output (including TVs). Default: 'id'.
+ * e.g. 'docFields=id,pagetitle,introtext'.
  * 
- * @param string $extenders - Comma-separated string determining which extenders should be applied to the snippet.
+ * @param string_commaSeparated $extenders - Comma-separated string determining which extenders should be applied to the snippet.
  * Be aware that the order of extender names can affect the output.
- * @param string $extendersParams - Parameters to be passed to their corresponding extensions. The parameter must be set as a query string,
+ * @param string_query $extendersParams - Parameters to be passed to their corresponding extensions. The parameter must be set as a query string,
+ * When $extenders == 'pagination' =>
+ * @param string_chunkName $extendersParams['wrapperTpl'] - Chunk to be used to output the pagination. @required
+ * @param string_chunkName $extendersParams['pageTpl'] - Chunk to be used to output pages within the pagination. @required
+ * @param string_chunkName $extendersParams['currentPageTpl'] - Chunk to be used to output the current page within the pagination. @required
+ * @param string_chunkName $extendersParams['nextTpl'] - Chunk to be used to output the navigation block to the next page. @required
+ * @param string_chunkName $extendersParams['nextOffTpl'] - Chunk to be used to output the navigation block to the next page if there are no more pages after. @required
+ * @param string_chunkName $extendersParams['previousTpl'] - Chunk to be used to output the navigation block to the previous page. @required
+ * @param string_chunkName $extendersParams['previousOffTpl'] - Chunk to be used to output the navigation block to the previous page if there are no more pages before. @required
  * e.g. $extendersParams = 'wrapperTpl=pagination&nextTpl=pagination_next&previousTpl=pagination_previous&nextOffTpl=pagination_nextOff&previousOffTpl=pagination_previousOff&pageTpl=pagination_page&currentPageTpl=pagination_page_current'.
  **/
 
